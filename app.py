64da09b7716ec457684d38ff05c18cb291132509
Vallakki Vasisht <vallakkivasisht28@gmail.com>
2025-07-24 10:12:26 +0530

    test update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Vallakki Vasisht bosch!"
+    return "Vallakki Vasisht 123!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
